4dccb865c356c915c45a987f019d67a9f6fdc9cc
Robot.l <lbh9311@sina.com>
2023-10-22 17:41:20 +0800

    feat: add suspense

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,36 @@
-import { Inter } from 'next/font/google'
-import React from 'react'
+import { Inter } from "next/font/google";
+import React, { Suspense } from "react";
 import { SwapWidget, darkTheme } from "@uniswap/widgets";
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-const SwawpComp = dynamic(() => import('@/components/Swap'), {ssr: false})
+const SwawpComp = dynamic(() => import("@/components/Swap"), { ssr: false });
 
 export default function Home() {
-  const [count, setCount] = React.useState(0)
+  const [count, setCount] = React.useState(0);
   return (
     <main
       className={`flex min-h-screen flex-col items-center p-24 ${inter.className}`}
     >
       <p>{`count: ${count}`}</p>
-      <button onClick={() => {
-        setCount(count + 1)
-      }}>Add</button>
-      <button onClick={() => {
-        setCount(count - 1)
-      }
-      }>Subtract</button>
-
-      <SwawpComp />
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Add
+      </button>
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        Subtract
+      </button>
+      <Suspense fallback={<div>Loading...</div>}>
+        <SwawpComp />
+      </Suspense>
     </main>
-  )
+  );
 }
